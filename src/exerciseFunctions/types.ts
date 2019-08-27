ffee45a8725a97bed77e5a352444204c30973364
Javier Bauset <javibauset@gmail.com>
2019-08-27 10:32:02 +0200

    Reorganize exerciseFuntions

---
+++ src/exerciseFunctions/types.ts
@@ -0,0 +1,16 @@
+export type PromotionType = 'FOREVER' | 'TEMPORARY';
+
+export type Promotion = {
+	start_date: Date,
+	end_date: Date,
+	type: PromotionType,
+	images: Buffer[],
+};
+
+export interface Functions {
+	getPromotionInstance (id: number): Promise<Promotion | null>;
+	resizeImage(image: Buffer): Buffer;
+	submitToGoogle(data: Promotion): Promise<{message: 'submitted'}>;
+ 	submitToFacebook(data: Promotion): Promise<{message: 'submitted'}>;
+ 	submitToYellowPages(data: Promotion): Promise<{message: 'submitted'}>;
+}
